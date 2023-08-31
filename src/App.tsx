d3f9bfbe3030e31f6ff137007b1b34ce3906cc2c
Kiril Shorin <kirillshorin8838@gmail.com>
2023-08-31 17:36:59 +0300

    add header

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/header';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <body>
+        
+      </body>
+    </>
+  );
+}
+
+export default App;
